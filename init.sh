bb5910492d171ffae3b69d2907e259f4ac175fb1
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2025-05-06 14:38:02 +0200

    some init changes for the logs but also keeping the rest

--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@ if ! psql -U odoo -h db -lqt | grep -q "^   odoo   |"; then
     echo "Database 'odoo' does not exist. Creating and initializing..."
     createdb -U odoo -h db odoo
     # For the first installation
-    odoo -d odoo -i point_of_sale,rabbitmq_heartbeat,user_delete,user_update,user_create,rabbitmq_orders,rabbitmq_logs --without-demo=all --addons-path=/mnt/extra-addons -u all --stop-after-init
+    odoo -d odoo -i point_of_sale,rabbitmq_heartbeat,user_delete,user_update,user_create,rabbitmq_orders,rabbitmq_logs,event_adder,event_consumer,order_adder --without-demo=all --addons-path=/mnt/extra-addons -u all --stop-after-init
 
 else
     echo "Database 'odoo' already exists. Skipping initialization."
     # For the update
-    odoo -d odoo -u point_of_sale,rabbitmq_heartbeat,user_delete,user_update,user_create,rabbitmq_orders,rabbitmq_logs --addons-path=/mnt/extra-addons --stop-after-init
+    odoo -d odoo -u point_of_sale,rabbitmq_heartbeat,user_delete,user_update,user_create,rabbitmq_orders,rabbitmq_logs,event_adder,event_consumer,order_adder --addons-path=/mnt/extra-addons --stop-after-init
 fi
 
 exec odoo -c /etc/odoo/odoo.conf
