9143b8a8b0e51dfb6be449d69edc798287487ccf
mathis-de-brouwer <145472006+mathis-de-brouwer@users.noreply.github.com>
2025-05-13 13:39:44 +0200

    deleting files that oare not used

--- custom-addons/event_adder/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Event Management',
-    'version': '1.0',
-    'depends': ['base'],
-    'author': 'Jorn De Smet',
-    'category': 'Custom',
-    'data': [], 
-    'installable': True,
-}
